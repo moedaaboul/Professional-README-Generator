8786dfc5da0aec1651b72895c9b668fd9fb7498e
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-01-03 00:13:51 +0000

    modularise questions.js

--- index.js
+++ index.js
@@ -1,57 +1,12 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const { writeToFile } = require("./utils/file");
+const { questions } = require("./utils/questions");
 
 // TODO: Create an array of questions for user input
-const questions = [
-  {
-    type: "input",
-    message: "What is the title of your project?",
-    name: "title",
-  },
-  {
-    type: "input",
-    message: "Enter description",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "Enter installation instructions",
-    name: "installation",
-  },
-  {
-    type: "input",
-    message: "Enter usage information",
-    name: "usage",
-  },
-  {
-    type: "input",
-    message: "Enter contribution guidlines",
-    name: "contributing",
-  },
-  {
-    type: "input",
-    message: "Enter test instructions",
-    name: "tests",
-  },
-  {
-    type: "input",
-    message: "Choose license",
-    name: "license",
-  },
-  {
-    type: "input",
-    message: "What is your github username?",
-    name: "username",
-  },
-  {
-    type: "input",
-    message: "What is your email address ? ",
-    name: "email",
-  },
-];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
---
+++ utils/questions.js
@@ -0,0 +1,49 @@
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Enter description",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Enter installation instructions",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Enter usage information",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Enter contribution guidlines",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "Enter test instructions",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "Choose license",
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "What is your github username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email address ? ",
+    name: "email",
+  },
+];
+
+module.exports = { questions };
